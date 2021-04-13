9b0d91b39f7fac3ee95f4d56d527c4b826a97e80
lmarello <leomarello@gmail.com>
2021-04-12 22:15:50 -0300

    add user model

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const { model, Schema } = require('mongoose')
+
+const userSchema = new Schema({
+  username: String,
+  name: String,
+  passwordHash: String,
+  notes: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Note',
+    },
+  ],
+})
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.passwordHash
+  },
+})
+
+const User = model('User', userSchema)
+
+module.exports = User
