5efe64a1c40083e621d2b89218bf0a5ae9be9328
lmarello <leomarello@gmail.com>
2021-04-12 22:29:55 -0300

    add user into noteSchema

--- models/Note.js
+++ models/Note.js
@@ -4,6 +4,10 @@ const noteSchema = new Schema({
   content: String,
   date: Date,
   important: Boolean,
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
 })
 
 noteSchema.set('toJSON', {
