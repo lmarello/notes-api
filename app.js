8c3774e165646f881a4b48e1378126b6ef1629f9
lmarello <leomarello@gmail.com>
2021-04-13 00:22:38 -0300

    use controller in app.js

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ require('./mongo')
 
 const express = require('express')
 const cors = require('cors')
-const Note = require('./models/Note')
 const Sentry = require('@sentry/node')
 const Tracing = require('@sentry/tracing')
 
 const notFound = require('./middlewares/notFound')
 const handleErrors = require('./middlewares/handleErrors')
+
+const usersRouter = require('./controllers/users')
+const notesRouter = require('./controllers/notes')
 
 const app = express()
 
@@ -35,79 +37,8 @@ app.use(Sentry.Handlers.requestHandler())
 
 app.use(Sentry.Handlers.tracingHandler())
 
-app.get('/api/notes', (request, response) => {
-  Note.find({})
-    .then((notes) => response.json(notes))
-    .catch((err) => {
-      console.log(err)
-      response.status(404).end()
-    })
-})
-
-app.get('/api/notes/:id', (request, response, next) => {
-  const { id } = request.params
-
-  Note.findById(id)
-    .then((note) =>
-      note ? response.status(201).json(note) : response.status(400).end()
-    )
-    .catch(next)
-})
-
-app.delete('/api/notes/:id', (request, response, next) => {
-  const { id } = request.params
-
-  Note.findByIdAndRemove(id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(next)
-})
-
-app.put('/api/notes/:id', (request, response, next) => {
-  const { id } = request.params
-  const note = request.body
-
-  const newNoteInfo = {
-    content: note.content,
-    important: note.important,
-  }
-
-  Note.findByIdAndUpdate(id, newNoteInfo, { new: true })
-    .then((newNote) => {
-      response.status(200).json(newNote)
-    })
-    .catch(next)
-})
-
-app.post('/api/notes', (request, response) => {
-  const { content, important } = request.body
-
-  if (!content) {
-    return response
-      .status(400)
-      .json({
-        error: 'Parameters missing',
-      })
-      .end()
-  }
-
-  const note = new Note({
-    content: content,
-    important: typeof important !== 'undefined' ? important : false,
-    date: new Date(),
-  })
-
-  note
-    .save()
-    .then((savedNote) => {
-      response.status(201).json(savedNote)
-    })
-    .catch((err) => {
-      console.log(err)
-      response.status(404).end()
-    })
-})
+app.use('/api/users', usersRouter)
+app.use('/api/notes', notesRouter)
 
 app.use(notFound)
 
